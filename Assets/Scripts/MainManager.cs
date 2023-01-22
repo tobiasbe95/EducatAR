ed49c322107bc23ae22f21af9341641e98740757
tobiasbe95 <tobias.beckmann95@gmail.com>
2023-01-22 22:16:53 +0100

    V0.2
    - added state machine for process flow (not fully functional)
    - updated scaling of objects based on testing with hololens using remote rendering for play mode
    - added azure object anchor for kitchen (not functional)

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainManager : MonoBehaviour
+{
+    Camera m_MainCamera;
+    
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        m_MainCamera = Camera.main;
+        m_MainCamera.enabled = true;
+    }
+
+    void Save()
+    {
+        PlayerPrefs.Save = m_MainCamera;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.LoadScene())
+        {
+
+        }
+    }
+    */
+}
